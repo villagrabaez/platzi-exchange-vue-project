b1b7970f4bbe532a6e1e73b68aaa38ec91dfb0fe
Bernardino Villagra <bernardinovillagra@hotmail.com>
2020-04-17 23:15:02 -0400

    Platzi exchange a producción!!!

---
+++ src/components/PxButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    v-on:click="buttonClick"
+    class="bg-trasnparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py- px-2 border border-green-500 hover:border-transparent rounded"
+  >
+    <beat-loader :loading="isLoading" :color="'#68d391'" :size="8" />
+    <span v-show="!isLoading">
+      <slot></slot>
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "PxButton",
+
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    buttonClick() {
+      this.$emit("custom-click");
+    }
+  }
+};
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">Platzi Exchange v1.0.0</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quo
+      facilis rerum ipsum ab iure vero, eaque minus vitae deserunt corporis
+      architecto id? Aut rem id, quas natus possimus aspernatur!
+    </p>
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline">
+      Volver a la página de inicio
+    </router-link>
+  </div>
+</template>
